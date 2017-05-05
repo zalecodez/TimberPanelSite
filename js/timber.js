170951044585d9d0d372cfd44166c95f24faf10f
zalecodez <zaleyoung@gmail.com>
2017-05-05 07:29:55 -0400

    fixing swipe sin jquerymobile

--- js/timber.js
+++ js/timber.js
@@ -9,6 +9,44 @@ function contactMap(){
     var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({position:latlng, map:map});
 }
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.originalEvent.touches[0].clientX;                                      
+    yDown = evt.originalEvent.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.originalEvent.touches[0].clientX;                                    
+    var yUp = evt.originalEvent.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+        } else { 
+            /* down swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+
 
 function search(){
 }
@@ -24,13 +62,40 @@ $(document).ready(function(){
         $("#timberNavbar.collapsing").css("cssText","height: "+navLength+"px!important;");
     });
 
+    $(".carousel").on('touchstart', handleTouchStart);        
+    $(".carousel").on('touchmove', function(evt){
+     if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.originalEvent.touches[0].clientX;                                    
+    var yUp = evt.originalEvent.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            $(this).carousel("next");
+        } else {
+            /* right swipe */
+            $(this).carousel("prev");
+        }                       
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;    
+    });
+
+/*
     $(".carousel").swipeleft(function() {
         $(this).carousel('next');
     });
     $(".carousel").swiperight(function() {
         $(this).carousel('prev');
     });
-
+*/
     
     if($("#timberCarousel").length)
         $("#timberCarousel").carousel({interval: 4000});
